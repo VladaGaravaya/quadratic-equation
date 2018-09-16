af62b923c55d89c3309b0dd4c58f148545d50f74
VladaGaravaya <36517813+VladaGaravaya@users.noreply.github.com>
2018-09-17 00:38:47 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var a,b,c='';
+    var arr=[];
+    arr=equation.split(' ');
+    a=+arr[0];
+    if(arr[3]=='-'){
+        arr[4]='-'+arr[4];
+    }
+    if(arr[7]=='-'){
+        arr[8]='-'+arr[8];
+    }
+    b=+arr[4];
+    c=+arr[8];
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    return [Math.round((-b-Math.sqrt(b*b-4*a*c))/(2*a)),Math.round((-b+Math.sqrt(b*b-4*a*c))/(2*a))].sort(compareNumbers);
 }
